73a13ed2409322a57e87c39bea836ca1a91d3af8
Timoth3Polo <timoth3.paul@gmail.com>
2022-11-26 14:37:54 +0100

    Add QuickStart to project

---
+++ QuickStart/Main.java
@@ -0,0 +1,13 @@
+package QuickStart;
+
+public class Main {
+
+    /*
+     * To compile code : javac QuickStart/Main.java
+     * To execute compiled code : java QuickStart.Main
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
